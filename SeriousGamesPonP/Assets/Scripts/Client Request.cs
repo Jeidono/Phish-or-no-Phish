475a47df8c6b4da7d5aa382ac11bc2d5556a106a
BDragonSoldier <ricky.oberholzer@gmail.com>
2025-08-22 16:24:59 +1000

    Some auto Functionality added

--- SeriousGamesPonP/Assets/Scripts/Client Request.cs
+++ SeriousGamesPonP/Assets/Scripts/Client Request.cs
@@ -1,7 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-
+[ExecuteAlways]
 public class ClientRequest : MonoBehaviour
 {
     public Sprite profilePic;
@@ -12,20 +12,59 @@ public class ClientRequest : MonoBehaviour
     public GameObject Content;
 
     [SerializeField] private Image Pic;
-    [SerializeField] private TextMeshPro Name;
-    [SerializeField] private TextMeshPro Age;
-    [SerializeField] private TextMeshPro Location;
+    [SerializeField] private TMP_Text Name;
+    [SerializeField] private TMP_Text Age;
+    [SerializeField] private TMP_Text Location;
     [SerializeField] private GameObject ContentHolder;
-    
+
+    private RectTransform ContCanv;
+
+    private RectTransform myCanv;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (Content)
+        {
+            ContCanv = Content.GetComponentInChildren<Canvas>().GetComponent<RectTransform>();
+        }
+
+        myCanv = gameObject.GetComponentInChildren<Canvas>().GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Pic&&profilePic)
+        {
+            Pic.sprite = profilePic;
+        }
+
+        if (Name)
+        {
+            Name.text = clientName;
+        }
+
+        if (Age)
+        {
+            Age.text = "Age: " + age;
+        }
+
+        if (Location)
+        {
+            Location.text = "Location:\n" + location.HouseNum + " " + location.StreetName + ",\n" + location.Suburb +
+                            ",\n" + location.Country;
+        }
+
+        if (ContentHolder && Content)
+        {
+            if (ContCanv.sizeDelta.x > 48)
+            {
+                myCanv.sizeDelta = new Vector2(ContCanv.sizeDelta.x + 2,ContCanv.sizeDelta.y+10);
+            }
+            else
+            {
+                myCanv.sizeDelta = new Vector2(50,ContCanv.sizeDelta.y+10);
+            }
+        }
     }
 }
