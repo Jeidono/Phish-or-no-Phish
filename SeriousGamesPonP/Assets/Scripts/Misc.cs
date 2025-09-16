d4e45f4599cf1b68636d52b2014ce46d5e8d8cf2
BDragonSoldier <ricky.oberholzer@gmail.com>
2025-09-16 15:24:42 +1000

    Person class in misc

--- SeriousGamesPonP/Assets/Scripts/Misc.cs
+++ SeriousGamesPonP/Assets/Scripts/Misc.cs
@@ -13,3 +13,12 @@ public struct Email
     public string username;
     public string domainName;
 }
+
+public class Person
+{
+    public string FName { get; private set;}
+    public string MName { get; private set;}
+    public string LName { get; private set;}
+    public int Age { get; private set;}
+    
+}
