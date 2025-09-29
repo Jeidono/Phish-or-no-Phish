c6165b70dbd990497cf22c9bfcd5eca2d453fadb
Jay <Byrnejay2001@gmail.com>
2025-09-29 20:48:55 +1000

    Button Script Cont / Temp BackgroundArt Demo functionality

--- SeriousGamesPonP/Assets/Scripts/MenuScripts/ButtonsScript.cs
+++ SeriousGamesPonP/Assets/Scripts/MenuScripts/ButtonsScript.cs
@@ -7,4 +7,27 @@ public class ButtonsScript : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void RestartAllScenes()
+    {
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene scene = SceneManager.GetSceneAt(i);
+            if (scene != SceneManager.GetActiveScene())
+            {
+                SceneManager.UnloadSceneAsync(scene);
+            }
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void mainmenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
 }
